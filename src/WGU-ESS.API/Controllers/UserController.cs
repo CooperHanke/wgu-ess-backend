3fe7da9445045052eddd623b50b87a4dc7ce0408
Cooper Hanke <cooper.hanke@gmail.com>
2021-06-26 12:02:41 -0500

    implemented logic for editing user based on role

--- src/WGU-ESS.API/Controllers/UserController.cs
+++ src/WGU-ESS.API/Controllers/UserController.cs
@@ -5,6 +5,7 @@ using WGU_ESS.Domain.Requests.User;
 using System;
 using System.IdentityModel.Tokens.Jwt;
 using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace WGU_ESS.API.Controllers
 {
@@ -35,7 +36,7 @@ namespace WGU_ESS.API.Controllers
       return Ok(result);
     }
 
-    [Authorize]
+    [Authorize(Roles = "Manager")] // only a manager should be able to add a user
     [HttpPost]
     public async Task<IActionResult> Post(AddUserRequest request)
     {
@@ -48,16 +49,44 @@ namespace WGU_ESS.API.Controllers
     public async Task<IActionResult> Put(Guid id, EditUserRequest request)
     {
       request.Id = id;
-      var result = await _userService.EditUserAsync(request);
-      return Ok(result);
+
+      // if the user is regular, only accept password and dark mode preference
+      // however, if they are a manager, change all fields
+      if (User.HasClaim(ClaimTypes.Role, "Manager"))
+      {
+        var result = await _userService.EditUserAsync(request);
+        return Ok(result);
+      }
+      // we check and ensure that only a valid claim for the same user can edit a user
+      else if (User.FindFirstValue("UserId") == request.Id.ToString())
+      {
+        // first, we make a copy of the user as they are by retrieving the user
+        var user = await _userService.GetUserAsync(new GetUserRequest { Id = request.Id });
+        // next, we recreate the edit user request
+        var result = await _userService.EditUserAsync(new EditUserRequest
+        {
+          Id = user.Id,
+          FirstName = user.FirstName,
+          LastName = user.LastName,
+          UserName = user.UserName,
+          Password = request.Password,
+          UsesDarkMode = request.UsesDarkMode,
+          Type = user.Type,
+          IsLocked = user.IsLocked,
+          IsHidden = user.IsHidden
+        });
+        return Ok(result);
+      }
+      else {
+        return BadRequest("Current user doesn't have permission to change another user's password");
+      }
     }
 
-    // to authenticate
-    [HttpPost("auth")]
+    [HttpPost("auth")]  // to authenticate
     public async Task<IActionResult> SignIn(SignInRequest request)
     {
       var result = await _userService.AuthenticateUser(request);
-      
+
       if (result.Token != null)
       {
         return Ok(new JwtSecurityTokenHandler().WriteToken(result.Token));
--- src/WGU-ESS.Domain/Responses/User/UserResponse.cs
+++ src/WGU-ESS.Domain/Responses/User/UserResponse.cs
@@ -1,5 +1,5 @@
 using System;
-using WGU_ESS.Domain.Entities;
+using System.Text.Json.Serialization;
 
 namespace WGU_ESS.Domain.Responses.User
 {
@@ -9,6 +9,8 @@ namespace WGU_ESS.Domain.Responses.User
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string UserName { get; set; }
+    [JsonIgnore]
+    public string Password { get; set; }
     public string Type { get; set; }
     public bool UsesDarkMode { get; set; }
     public bool IsLocked { get; set; }
