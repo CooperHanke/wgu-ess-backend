12deb0e325be8037464eb3abc4470875882c4669
Cooper Hanke <cooper.hanke@gmail.com>
2021-07-09 22:17:33 -0500

    able to properly pass user guid

--- src/WGU-ESS.API/Controllers/ContactController.cs
+++ src/WGU-ESS.API/Controllers/ContactController.cs
@@ -26,7 +26,7 @@ namespace WGU_ESS.API.Controllers
       return Ok(result);
     }
 
-    [HttpGet("user/{id:guid}")]
+    [HttpGet("user/{userId:guid}")]
     public async Task<IActionResult> GetByUserId(Guid userId)
     {
       var result = await _contactService.GetContactsByUserIdAsync(new GetContactByUserIdRequest { UserId = userId } );
